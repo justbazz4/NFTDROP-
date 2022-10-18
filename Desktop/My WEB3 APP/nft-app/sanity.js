6d3cea9c99c1b70df15ebd6d184d35a445d15401
justbazz4 <unopposedpresident@gmail.com>
2022-10-18 21:14:33 +0800

    https://github.com/justbazz4/NFTDROP-

--- Desktop/My WEB3 APP/nft-app/sanity.js
+++ Desktop/My WEB3 APP/nft-app/sanity.js
@@ -2,8 +2,8 @@ import { createCurrentUserHook, createClient } from 'next-sanity';
 import createImageUrlBuilder from '@sanity/image-url';
 
 export const  config = {
-    dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || 'production',
-    projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+    dataset: process.env.REACT_APP_NEXT_PUBLIC_SANITY_DATASET || 'production',
+    projectId: process.env.REACT_APP_NEXT_PUBLIC_SANITY_PROJECT_ID,
     apiVersion: '2021-03-25',
     useCdn: process.env.NODE_ENV === 'production',
 }
